9454e08df3075cc46fd851f498faaed1fab4ba86
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-06-05 16:18:08 +0200

    push api

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.get('/animes', (req, res) => {
 })
 app.get('/animes/v1/:animeName', (req, res) => {
   const animeName = req.params.animeName;
+  console.log(animeName)
   const filePath = `/animes/${animeName}/persos.json`;
+  console.log(filePath)
   
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
